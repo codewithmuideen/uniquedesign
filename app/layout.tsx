80e039b9af81f2bc7f4c05a427e303e6e61b4329
Olawale <codewitmui@gmail.com>
2025-04-10 11:38:50 +0100

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "@/styles/globals.css";
 export const metadata: Metadata = {
   title: "Oluwatobi Olanipekun - Crafting Digital Experiences That Matter",
   description: "Oluwatobi Olanipekun - Crafting Digital Experiences That Matter",
+  icons: {
+    icon: "/favicon.ico", // You can also use png like "/favicon.png"
+  },
 };
 
 export default function RootLayout({
@@ -13,7 +16,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className="">{children}</body>
+      <head />
+      <body>{children}</body>
     </html>
   );
 }
